fe32540e4c77be08ab2f9ddb1c67b638246ce381
raymww <raymond.m.wang@gmail.com>
2024-09-24 23:30:51 -0500

    working?

--- src/main/java/edu/cs/utexas/HadoopEx/WordCountMapper.java
+++ src/main/java/edu/cs/utexas/HadoopEx/WordCountMapper.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class WordCountMapper extends Mapper<Object, Text, Text, FloatWritable> {
+public class WordCountMapper extends Mapper<Object, Text, Text, Wrapper> {
 
 	// Create a counter and initialize with 1
 	private final FloatWritable counter = new FloatWritable(1);
@@ -21,22 +21,14 @@ public class WordCountMapper extends Mapper<Object, Text, Text, FloatWritable> {
 		
 		String[] fields = value.toString().split(",");
 		try {
-				Float pickupLat = fields[6].trim().isEmpty() ? 0f : Float.parseFloat(fields[6].trim());
-				Float pickupLong = fields[7].trim().isEmpty() ? 0f : Float.parseFloat(fields[7].trim());
-				Float dropOffLat = fields[8].trim().isEmpty() ? 0f : Float.parseFloat(fields[8].trim());
-				Float dropOffLong = fields[9].trim().isEmpty() ? 0f : Float.parseFloat(fields[9].trim());
 				String taxiID = fields[0].trim();
-				float errors = 0;
+				float time = Float.parseFloat(fields[4]);
+				float total = Float.parseFloat(fields[16]);
 
-				if (Math.abs(pickupLat) < 0.001 || Math.abs(pickupLong) < 0.001 || Math.abs(dropOffLat) < 0.001 || Math.abs(dropOffLong) < 0.001) {
-					errors = 1;
-				}
 
-				// if (errors > 0) {
+
 				word.set(String.valueOf(taxiID));
-				counter.set(errors);
-				context.write(word, counter);
-				// }
+				context.write(word, new Wrapper(time, total));
 
 
 		} catch (Exception e) {
--- src/main/java/edu/cs/utexas/HadoopEx/WordCountReducer.java
+++ src/main/java/edu/cs/utexas/HadoopEx/WordCountReducer.java
@@ -9,49 +9,40 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordCountReducer extends  Reducer<Text, FloatWritable, Text, FloatWritable> {
+public class WordCountReducer extends  Reducer<Text, Wrapper, Text, FloatWritable> {
 
-   public void reduce(Text text, Iterable<FloatWritable> values, Context context)
+   public void reduce(Text text, Iterable<Wrapper> values, Context context)
            throws IOException, InterruptedException {
 	   
-       HashMap<String, float[]> taxiData = new HashMap<>(); // error, count
-        PriorityQueue<String[]> errors = new PriorityQueue<>((a, b) -> Float.compare(Float.parseFloat(a[1]), Float.parseFloat(b[1])));
+        HashMap<String, float[]> taxiData = new HashMap<>(); // error, count
 
-       
-        for (FloatWritable val : values) {
+        
+        for (Wrapper val : values) {
             
-            float value = val.get();
+            float time = val.gettime().get();
+            float total = val.gettotal().get();
             String taxiID = text.toString();
-            // System.out.println("Taxi ID: " + taxiID + " Errors: " + value);
 
             if (taxiData.containsKey(taxiID)) {
                 float[] data = taxiData.get(taxiID);
-                data[0] += value;
-                data[1] += 1;
+                data[0] += time;
+                data[1] += total;
                 taxiData.put(taxiID, data);
             } else {
                 float[] data = new float[2];
-                data[0] = value;
-                data[1] = 1;
+                data[0] = time;
+                data[1] = total;
                 taxiData.put(taxiID, data);
 
             }
         }
         for (String taxi : taxiData.keySet()) {
             float[] data = taxiData.get(taxi);
-            float averageError = 0;
+            float moneypmin = 0;
             if (data[0] != 0 && data[1] != 0) {
-                averageError = data[0] / data[1];
+                moneypmin = data[1] / (data[0] * 60);
             }
-            errors.add(new String[]{taxi, String.valueOf(averageError)});
-            if (errors.size() > 5) {
-                errors.poll();
-            }
-            context.write(new Text(taxi), new FloatWritable(averageError));
+            context.write(new Text(taxi), new FloatWritable(moneypmin));
         }
-        // for (String[] error : errors) {
-        //     System.out.println("Taxi ID: " + error[0] + " Average Error: " + error[1]);
-        // }
-       
    }
 }
