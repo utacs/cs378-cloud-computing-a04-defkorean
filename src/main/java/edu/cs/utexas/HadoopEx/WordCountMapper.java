1127df723f356d97d6abbb5d279af5d1362654e1
nathanjacob <37455968+nathan-jacob@users.noreply.github.com>
2024-09-24 21:35:16 -0500

    raymond get driving

--- src/main/java/edu/cs/utexas/HadoopEx/WordCountMapper.java
+++ src/main/java/edu/cs/utexas/HadoopEx/WordCountMapper.java
@@ -2,6 +2,8 @@ package edu.cs.utexas.HadoopEx;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -17,10 +19,55 @@ public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
 	public void map(Object key, Text value, Context context) 
 			throws IOException, InterruptedException {
 		
-		StringTokenizer itr = new StringTokenizer(value.toString());
-		while (itr.hasMoreTokens()) {
-			word.set(itr.nextToken());
-			context.write(word, counter);
+		String[] fields = value.toString().split(",");
+		try {
+				String pickupTime = fields[2].trim();
+				String dropoffTime = fields[3].trim();
+				Float pickupLat = Float.parseFloat(fields[6].trim());
+				Float pickupLong = Float.parseFloat(fields[7].trim());
+				Float dropoffLat = Float.parseFloat(fields[8].trim());
+				Float dropoffLong = Float.parseFloat(fields[9].trim());
+				
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+				LocalDateTime pickupDateTime = LocalDateTime.parse(pickupTime, formatter);
+
+				int pickupHour = pickupDateTime.getHour();
+				int dropOffHour = LocalDateTime.parse(dropoffTime, formatter).getHour();
+
+				int pickUpErrors = 0;
+				int dropOffErrors = 0;
+
+				if (pickupLat == 0) {
+					pickUpErrors++;
+				}
+				if (pickupLong == 0) {
+					pickUpErrors++;
+				}
+				if (dropoffLat == 0) {
+					dropOffErrors++;
+				}
+				if (dropoffLong == 0) {
+					dropOffErrors++;
+				}
+				if (pickUpErrors > 1) {
+					word.set(String.valueOf(pickupHour));
+					counter.set(pickUpErrors);
+					context.write(word, counter);
+				}
+				if (dropOffErrors > 1) {
+					word.set(String.valueOf(dropOffHour));
+					counter.set(dropOffErrors);
+					context.write(word, counter);
+				}
+
+
+		} catch (Exception e) {
+			// Catch-all for any other exceptions
+			return;
 		}
+		// while (itr.hasMoreTokens()) {
+		// 	word.set(itr.nextToken());
+		// 	context.write(word, counter);
+		// }
 	}
 }
