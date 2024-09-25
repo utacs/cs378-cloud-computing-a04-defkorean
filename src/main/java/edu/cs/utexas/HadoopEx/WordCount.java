eb4daf4b19211746e8924efe549f7e176577e21f
raymww <raymond.m.wang@gmail.com>
2024-09-24 22:41:23 -0500

    added second mapreduce

--- src/main/java/edu/cs/utexas/HadoopEx/WordCount.java
+++ src/main/java/edu/cs/utexas/HadoopEx/WordCount.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
@@ -55,7 +56,34 @@ public class WordCount extends Configured implements Tool {
 			FileOutputFormat.setOutputPath(job, new Path(args[1]));
 			job.setOutputFormatClass(TextOutputFormat.class);
 
-			return (job.waitForCompletion(true) ? 0 : 1);
+			if (!job.waitForCompletion(true)) {
+				return 1;
+			}
+
+			Job job2 = new Job(conf, "TopK");
+			job2.setJarByClass(WordCount.class);
+
+			// specify a Mapper
+			job2.setMapperClass(TopKMapper.class);
+
+			// specify a Reducer
+			job2.setReducerClass(TopKReducer.class);
+
+			// specify output types
+			job2.setOutputKeyClass(Text.class);
+			job2.setOutputValueClass(FloatWritable.class);
+
+			// set the number of reducer to 1
+			job2.setNumReduceTasks(1);
+
+			// specify input and output directories
+			FileInputFormat.addInputPath(job2, new Path(args[1]));
+			job2.setInputFormatClass(KeyValueTextInputFormat.class);
+
+			FileOutputFormat.setOutputPath(job2, new Path(args[2]));
+			job2.setOutputFormatClass(TextOutputFormat.class);
+
+			return (job2.waitForCompletion(true) ? 0 : 1);
 
 		} catch (InterruptedException | ClassNotFoundException | IOException e) {
 			System.err.println("Error during mapreduce job.");
